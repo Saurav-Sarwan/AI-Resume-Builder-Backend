2541f20e1cfe86cb5b5a13fd03cb378b7cbff93e
Saurav Sarwan <sauravsarwan1234@gmail.com>
2026-05-17 13:20:52 +0530

    fix the cors issue

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
-const express = require("express")
-const cookieParser = require("cookie-parser") //middleware to read cookies from request
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()   // initiate server
+const app = express();
 
-app.use(express.json()) // allows to read req.body
-app.use(cookieParser()) // allows to read cookies from request
+app.use(express.json());
+app.use(cookieParser());
+
+const allowedOrigin = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
+    origin: allowedOrigin,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.options("*", cors());
 
-/* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
-/* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-module.exports = app
+module.exports = app;
