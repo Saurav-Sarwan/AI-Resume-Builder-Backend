2db1f933a08c5b201791cfb3f3afc8faf2f4451f
Saurav Sarwan <sauravsarwan1234@gmail.com>
2026-05-17 13:23:53 +0530

    fix the cors issue

--- src/app.js
+++ src/app.js
@@ -7,16 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigin = process.env.FRONTEND_URL;
-
 app.use(cors({
-    origin: allowedOrigin,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-app.options("*", cors());
 
 const authRouter = require("./routes/auth.routes");
 const interviewRouter = require("./routes/interview.routes");
