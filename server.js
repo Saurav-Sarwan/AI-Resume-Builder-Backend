0d16523b089468d96f7d6a24caa27d571fb74182
Saurav Sarwan <sauravsarwan1234@gmail.com>
2026-05-17 13:32:57 +0530

    fix the server issue

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require("dotenv").config() //through this method we can call all variables present under .env file
+require("dotenv").config();
 
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
-connectToDB()
+connectToDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on ${PORT}`);
 });
